22f6784ce1404d9ea0a6b5cdf03b2946e2a98348
shamsWMM <shams.alsaegh@gmail.com>
2025-11-10 23:03:15 +1100

    Add card component

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import "./Card.css";
+
+const Card = () => {
+    return (
+        <div className="card">
+            <img
+                src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                alt="Image"
+            />
+            <div className="details">
+                <h2>APPl</h2>
+                <p>$48484</p>
+            </div>
+            <p className="info">
+                Lorem ipsum dolor, sit amet consectetur.
+            </p>
+        </div>);
+};
+
+export default Card;
