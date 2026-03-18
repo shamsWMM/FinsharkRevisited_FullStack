f75faaeefca23bf0566ec69822545227b0ea29ec
swatha <shamswathas@gmail.com>
2026-03-18 22:02:06 +1100

    add context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css"
 import Navbar from "./components/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./context/useAuth";
 
 function App() {
   return (
     <>
+    <UserProvider>
     <Navbar />
     <Outlet />
     <ToastContainer />
+    </UserProvider>
     </>
   );
 };
